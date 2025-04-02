5b205469bd9cde02806322abba34cb492a184359
Kokoss <trantuanmanh.kokoss@gmail.com>
2025-04-02 15:05:35 +0700

    init config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymaps = vim.keymap.set
+local opts = { silent = true, noremap = true }
+keymaps("i", "jk", "<ESC>")
+keymaps("n", "<TAB>", ":tabnext<CR>", opts)
+keymaps("n", "<S-TAB>", ":tabprev<CR>", opts)
+keymaps("n", "=", "<C-a>", opts)
+keymaps("n", "-", "<C-x>", opts)
+keymaps("i", "<ESC>", "`")
